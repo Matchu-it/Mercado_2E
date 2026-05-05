ff521625783c2e991889ed4ca9bf826a0910c3fd
James Mercado <jamesmatthewmercado31@gmail.com>
2026-05-05 23:15:12 +0800

    my first commit

--- activity.sh
+++ activity.sh
@@ -1,1 +1,6 @@
-
+echo"Enter your name:"
+echo"Enter your folder name:"
+mkdir activty
+cd activity
+echo "Oen-Source Technologies" > readme.txt 
+echo "Hello, [James Matthew Mercado]! your folder [activity] has been successfully created"
